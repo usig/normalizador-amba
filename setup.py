f57e10a7f20131e9d815c3cc0eed06d332f771c2
Hernan Ogasawara <hogasawara@buenosaires.gob.ar>
2016-07-01 11:41:07 -0300

    = Version publicable + Nuevo setup.py

--- setup.py
+++ setup.py
@@ -1,91 +1,37 @@
 # coding: UTF-8
 #!/usr/bin/env python
 from __future__ import absolute_import
-from distutils.core import setup
-from distutils.command.install_data import install_data
-from distutils.command.install import INSTALL_SCHEMES
-import os
-import sys
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-sys.path.append('normalizador_direcciones_amba')
+import usig_normalizador_amba
 
-import normalizador_direcciones_amba
-
-__version__ = normalizador_direcciones_amba.VERSION
-
-class osx_install_data(install_data):
-    # On MacOS, the platform-specific lib dir is /System/Library/Framework/Python/.../
-    # which is wrong. Python 2.5 supplied with MacOS 10.5 has an Apple-specific fix
-    # for this in distutils.command.install_data#306. It fixes install_lib but not
-    # install_data, which is why we roll our own install_data class.
-
-    def finalize_options(self):
-        # By the time finalize_options is called, install.install_lib is set to the
-        # fixed directory, so we set the installdir to install_lib. The
-        # install_data class uses ('install_data', 'install_dir') instead.
-        self.set_undefined_options('install', ('install_lib', 'install_dir'))
-        install_data.finalize_options(self)
-
-if sys.platform == "darwin": 
-    cmdclasses = {'install_data': osx_install_data} 
-else: 
-    cmdclasses = {'install_data': install_data}
-
-
-
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
-
-# Tell distutils to put the data_files in platform-specific installation
-# locations. See here for an explanation:
-# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
-
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir != '':
-    os.chdir(root_dir)
-app_dir = 'normalizador_direcciones_amba'
-
-for dirpath, dirnames, filenames in os.walk(app_dir):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        packages.append('.'.join(fullsplit(dirpath)))
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
-
-data_files.append(['', ['README.md']])
-
-# Small hack for working with bdist_wininst.
-# See http://mail.python.org/pipermail/distutils-sig/2004-August/004134.html
-if len(sys.argv) > 1 and sys.argv[1] == 'bdist_wininst':
-    for file_info in data_files:
-        file_info[0] = '\\PURELIB\\%s' % file_info[0]
-#raise Exception(packages)
+with open('README.rst', 'r') as f:
+    readme = f.read()
 
 setup(
-    name = "normalizador_direcciones_amba",
-    version = __version__.replace(' ', '_'),
-    author = 'USIG',
-    author_email = 'hogasawara@buenosaires.gob.ar',
-    description = u'Normalizador de Direcciones del Área Metropolitana de Buenos Aires',
-    packages = packages,
-    cmdclass = cmdclasses,
-    data_files = data_files,
+    name = usig_normalizador_amba.__title__,
+    version = usig_normalizador_amba.__version__,
+    description = usig_normalizador_amba.__description__,
+    long_description = readme,
+    author = usig_normalizador_amba.__author__,
+    author_email = usig_normalizador_amba.__author_email__,
+    url='https://github.com/usig/normalizador-amba',
+    license = usig_normalizador_amba.__license__,
+    packages = ['usig_normalizador_amba', 'tests'],
+    package_data = {'tests': ['callejeros/*']},
+    keywords = 'usig gcba gis normalizador direcciones amba',
+    platforms = ['Unix/Linux'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Spanish',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: GIS',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
